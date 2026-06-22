4dec554ecdeed615a36c28b92c55bd241b7a5bb7
asag22 <letypdiwad@gmail.com>
2026-06-23 00:05:16 +0200

    sesja znika z bazy po wylogowaniu

--- strona-www/src/routes/logout/+page.server.ts
+++ strona-www/src/routes/logout/+page.server.ts
@@ -1,8 +1,18 @@
+import { invalidateSession } from '$lib/server/auth';
 import { redirect, type Actions } from '@sveltejs/kit';
 
 export const actions: Actions = {
     default: async ({ cookies, locals }) => {
+        const sessionId = cookies.get("session");
+
         cookies.delete("session", {path: "/"});
+
+
+        if(!sessionId) {
+            throw redirect(303, "/");    
+        }
+        
+        invalidateSession(sessionId);
 
         locals.user = null;
 
